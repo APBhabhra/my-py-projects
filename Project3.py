73fd241df3a349af58e4e6ef76ce0a63cbdf617a
APBhabhra <73386408+APBhabhra@users.noreply.github.com>
2020-10-30 09:15:59 +0000

    Add files via upload
    Project 3

---
+++ Project3.py
@@ -0,0 +1,8 @@
+filename1=int(input("enter file name"))
+filename2=int(input("enter file name"))
+filename1= open("sample1.txt","a+")
+filename2= open("sample2.txt","a+")
+filename1= data_a
+filename2= data_b
+filename1= open("sample1.txt","w")= data_b
+filename2= open("sample2.txt"."w")= data_a
