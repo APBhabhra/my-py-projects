3d210ac3052a00bb6203cb93c9ecf51154893343
APBhabhra <73386408+APBhabhra@users.noreply.github.com>
2020-11-21 19:04:12 +0000

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,12 @@
+class atm
+atm=int(input("Enter your card number))
+atm=int(input("Enter your pin number"))
+atm=input("pick a procedure")
+if(CashWithdrawl):
+    Print("Money Widthdrawed")
+elif(Pin change):
+    Print("ok pin has been changed")
+elif(other services):
+    print("ok connecting to your local bank")
+else():
+    print("sorry could not find what you were looking for")
